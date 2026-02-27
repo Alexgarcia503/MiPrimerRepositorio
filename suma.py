c43b18f2e78e3eb8d3c5e9b0627238473190d6b2
Alexgarcia503 <alexcarballo471@gmail.com>
2026-02-26 20:03:51 -0600

    Se agrego el primer ejercicio de suma en python

---
+++ suma.py
@@ -0,0 +1,44 @@
+# es un comentario
+
+## para declarar una variable
+nombre = "Alex"
+print(nombre)
+
+edad = 40
+
+print(nombre, "tiene", edad,  "años") ## concatenar
+# binario = 0
+# binario = 1
+
+
+
+# string = letras
+# objeto = 012345
+
+
+## direccion # 0x
+## tipo => numerico o string ( booleano, objeto)
+## tipo numero => prepara la memoria
+## para guardar un tippo de dato string exa.
+
+## textos largos vamos a utilizar
+texto_largo = """
+tjwrgtrwjikgoethbjogkttekgh,
+egjiekogetgkmetoget.
+etgikoetgl. """
+print(texto_largo)
+## Las comillas triples se usan para textos largos
+# mantiene el formato de lo que se copia entre ellas
+## al momento de compilarlas
+
+numero1 = 10
+numero2 = 20
+
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 / numero2
+print("El resultado de la suma es:", resultado)
+
+resultado = numero1 * numero2
+print("El resultado de la suma es:", resultado)
