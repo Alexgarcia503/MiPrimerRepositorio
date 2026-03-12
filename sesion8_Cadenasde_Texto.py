bba89685355e7113f00157ea85aa32f6c074bd5f
Alexgarcia503 <alexcarballo471@gmail.com>
2026-03-12 17:54:05 -0600

    Se agrega un ejemplo para definir las mayusculas y minusculas en python

---
+++ sesion8_Cadenasde_Texto.py
@@ -0,0 +1,53 @@
+# la comillas triples son las que permiten escribir en varias lineas
+
+# Textos Largos
+poema = """La noche está estrellada,
+y tiritan, azules, los astros, a lo lejos".
+El viento de la noche gira en el cielo y canta.
+Puedo escribir los versos más tristes esta noche.
+Yo la quise, y a veces ella también me quiso.
+En las noches como ésta la tuve entre mis brazos.
+La besé tantas veces bajo el cielo infinito.
+Ella me quiso, a veces yo también la quería.
+Cómo no haber amado sus grandes ojos fijos.
+Puedo escribir los versos más tristes esta noche.
+Pensar que no la tengo. Sentir que la he perdido.
+Oír la noche inmensa, más inmensa sin ella.
+Y el verso cae al alma como al pasto el rocío.
+Qué importa que mi amor no pudiera guardarla.
+La noche está estrellada y ella no está conmigo."""
+
+## print(poema)
+
+## computadora -> que variable queres imprimir
+## print() =>
+# void -> no devuelve nada
+# objeto -> devuelve un tipo de dato
+
+## realizar una wiki tambien puede darle doble click documento y
+## se les despliega el editor de texto
+
+## MAYUSCULAS
+## multabilidad -> siempre debemos evitar tansformar objeto original
+## clases -> estereotipo (como un molde)
+## propiedades ->
+## color
+# tipo de motor ( electricos o de gas)
+# Ojos
+# color de pelo
+
+# moverse
+# frenar
+# cargarse
+# descargarse
+
+# poema es un espacio de memoria para string
+# se va llenar el contenido de poema alterar con al accion Upper
+## poema_Mayusculas = poema.upper()
+## print(poema_Mayusculas)
+
+# convertir en minusculas
+## string .lower
+
+poema_minuscula = poema.lower()
+print(poema_minuscula)
