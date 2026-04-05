fcb43cda0853f0fb08f387a6a3a70f4b3036f32a
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-05 12:44:41 -0600

    Se agregó el ejercicio 1 que solicita el nombre completo y muestra mayúsculas, minúsculas y cantidad de caracteres

---
+++ EjerciciosPythonS9/Ejercicio1.py
@@ -0,0 +1,14 @@
+# pedir nombre
+nombre = input("Ingrese su nombre: ")
+
+# MAYUSCULAS
+nombre_mayusculas = nombre.upper()
+print(nombre_mayusculas)
+
+# minusculas
+nombre_minusculas = nombre.lower()
+print(nombre_minusculas)
+
+# cantidad
+cantidad = len(nombre)
+print(cantidad)
