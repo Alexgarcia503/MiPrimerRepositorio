be12accdb8171584166cf98b036ded35ac1a20cf
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-09 19:09:32 -0600

    clase 09/04/26

---
+++ python/semana11/buclewhile.py
@@ -0,0 +1,10 @@
+# si tenemos una variable y necesitamos comprobar
+
+clima = "caliente"  ## clima por defecto
+entrada = input("¿Como esta el clima?")
+
+
+## print("El clima es: ", entrada)
+
+if entrada == "frio":
+    print("comprar Al. de azucar ")
