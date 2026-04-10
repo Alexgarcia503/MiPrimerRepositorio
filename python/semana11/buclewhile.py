e402e92b276a01dd7252ffb2698445044cd40b8f
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-09 19:18:44 -0600

    Completar ejercicio de bucle while en semana 11

--- python/semana11/buclewhile.py
+++ python/semana11/buclewhile.py
@@ -1,10 +1,22 @@
 # si tenemos una variable y necesitamos comprobar
 
-clima = "caliente"  ## clima por defecto
-entrada = input("¿Como esta el clima?")
+## clima = "caliente"  ## clima por defecto
+## entrada = input("¿Como esta el clima?")
 
 
 ## print("El clima es: ", entrada)
 
-if entrada == "frio":
-    print("comprar Al. de azucar ")
+## if entrada == "frio":
+   ## print("comprar Al. de azucar ")
+
+if numeroComparar == False:
+    print("debes de trabajar para comprar u nterreno")
+else:
+    print("Debes cuidar tus rodillas")
+
+    if numer2 > 24 and numero <30:
+        print("El numero es mayor a 24 y menor a 30")
+elif numer2 > 30:
+    print("El numero es mayor a 30")
+else:
+
