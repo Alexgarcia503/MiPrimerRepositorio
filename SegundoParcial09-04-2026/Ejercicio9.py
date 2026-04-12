6945413e8c2d2732009a02cc6c5925941228c110
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-11 18:02:15 -0600

    Ejercicio 9: Reemplazo exacto de texto con replace() y conversión a mayúsculas con upper()

---
+++ SegundoParcial09-04-2026/Ejercicio9.py
@@ -0,0 +1,11 @@
+# 1. Cadena inicial
+texto = "Any time y Anytime"
+
+# 2. Reemplazar "Any time" por "Always"
+texto_reemplazado = texto.replace("Any time", "Always")
+
+# 3. Convertir todo a mayúsculas
+texto_final = texto_reemplazado.upper()
+
+# Mostrar resultado
+print("Texto final:", texto_final)
