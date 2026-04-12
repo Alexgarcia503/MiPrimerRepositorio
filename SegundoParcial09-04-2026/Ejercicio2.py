cdbb59e0f5fbe2de62bf954f346dc38adc039b90
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-11 18:00:08 -0600

    Ejercicio 2: Formateo de texto con title() y reemplazo de 'Su Nombre' por 'Su Apellido' usando replace()

---
+++ SegundoParcial09-04-2026/Ejercicio2.py
@@ -0,0 +1,11 @@
+# 1. Cadena de texto
+texto = "Alex"
+
+# 2. Convertir la primera letra de cada palabra en mayúscula
+texto_formateado = texto.title()
+
+# 3. Reemplazar "Su Nombre" por "Su Apellido"
+texto_final = texto_formateado.replace("Su Nombre", "Su Apellido")
+
+# Mostrar resultado
+print("Texto final:", texto_final)
