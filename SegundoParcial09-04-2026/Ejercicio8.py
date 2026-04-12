b711baf8ff72468285f9838d70e195332a1aa04e
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-11 18:01:57 -0600

    Ejercicio 8: Definición de texto multilínea, conteo de caracteres con count() y separación en lista con splitlines()

---
+++ SegundoParcial09-04-2026/Ejercicio8.py
@@ -0,0 +1,14 @@
+# 1. Bloque de texto de 3 líneas
+texto = """Amanecer en el valle
+la brisa acaricia el alma
+canta la naturaleza"""
+
+# 2. Contar cuántas veces aparece la letra "a"
+cantidad_a = texto.count("a")
+
+# 3. Dividir el texto por saltos de línea
+lista_oraciones = texto.splitlines()
+
+# Mostrar resultados
+print("Cantidad de 'a':", cantidad_a)
+print("Lista de oraciones:", lista_oraciones)
