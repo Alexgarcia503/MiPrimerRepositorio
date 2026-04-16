c525c78ce9ea9250496072bab50a6a52060c5d71
Alexgarcia503 <alexcarballo471@gmail.com>
2026-04-15 21:27:31 -0600

    Ejercicio 7: Aplicación de múltiples transformaciones a un texto usando una lista de opciones

---
+++ LaboratorioS10/Ejercicio7.py
@@ -0,0 +1,15 @@
+def transformar_multiple(texto, lista_numeros):
+    for numero in lista_numeros:
+        if numero == 1:
+            texto = texto.upper()
+        elif numero == 2:
+            texto = texto.lower()
+        elif numero == 3:
+            texto = texto.capitalize()
+        else:
+            return "Opción inválida"
+
+    return texto
+
+
+print(transformar_multiple("python", [1, 3, 2]))
