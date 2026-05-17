c65012362eb269abaad7b616578ee8f9af8595bb
Alexgarcia503 <alexcarballo471@gmail.com>
2026-05-17 09:05:04 -0600

    se añado semana 13 de clases

---
+++ SEMANA13/juego.py
@@ -0,0 +1,7 @@
+def solicitarPalabra ():
+    palabra = input ("Ingrese una palabra")
+    palabra =  palabra.lower()
+    return palabra
+
+def ListaACompletar(palabraAcomparar):
+    ListaACompletar[] = len(palabraAcomparar)
